2394d0fafc01ce8ab127021eec3b14cae0511b8a
Sindhuja Baskar <153032345+sindhujabaskar@users.noreply.github.com>
2025-04-11 18:18:55 -0400

    Feat/Fix: added tests for ensuring data filter steps work; converted baseline percentile function to ouptut array instead of list

--- py2p/calculate.py
+++ py2p/calculate.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 
 def calculate_baseline(filtered_roi, percentile): # calculates the specified percentile along each roi's raw fluorescence to yield a baseline fluorescence value
-    percentile_list = []
+    baseline_percentile = np.array([])
     for row in filtered_roi:
         find_percentile = np.percentile(row, percentile, keepdims = True)
-        percentile_list.append(find_percentile)
-    return percentile_list
+        np.append(baseline_percentile, find_percentile)
+    return baseline_percentile
 
 def calculate_dff(raw_f, baseline_f): # calculates the dff values for each roi based on a calculated baseline fluorescence
     dff_list = []
--- py2p/main.py
+++ py2p/main.py
@@ -5,13 +5,15 @@ from py2p.config import DATA_DIR
 
 def main():
     data = load_suite2p_outputs(DATA_DIR)
-    print('Hello', data)
 
     filtered_data = filter_rois(data)
-    print('Filtered Data:', filtered_data)
-    
-    baseline_fluorescence = calculate_baseline(filtered_data, percentile = 10)
-    print(baseline_fluorescence)
+    #assert filtered_data[0].shape == data['roi_fluorescence'].shape, "Filtered data shape does not match original data shape."
+    # Filtered data returns a tuple of (true_cells_only, filtered_roi, filtered_neuropil) where true_cells_only contains
+    # numpy booleans (not Python booleans) so we can use sum 
+    assert filtered_data[0].sum() == filtered_data[1].shape[0], "Filtered data does not match the number of true cells."
+
+    baseline_fluorescence = calculate_baseline(filtered_data[1], percentile = 3)
+    assert baseline_fluorescence == filtered_data[1][1]  
 
 if __name__ == "__main__":
     main()
