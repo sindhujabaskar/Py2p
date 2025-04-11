6d057f66220d0c49fa9607b4ba6427ca71dea749
Sindhuja Baskar <153032345+sindhujabaskar@users.noreply.github.com>
2025-04-11 15:15:53 -0400

    Feat: added calculation funtions for baseline and dff

--- py2p/main.py
+++ py2p/main.py
@@ -1,11 +1,17 @@
 from py2p.io import load_suite2p_outputs
+from py2p.transform import filter_rois
+from py2p.calculate import calculate_baseline, calculate_dff
 from py2p.config import DATA_DIR
 
 def main():
     data = load_suite2p_outputs(DATA_DIR)
-    
     print('Hello', data)
 
+    filtered_data = filter_rois(data)
+    print('Filtered Data:', filtered_data)
+    
+    baseline_fluorescence = calculate_baseline(filtered_data, percentile = 10)
+    print(baseline_fluorescence)
 
 if __name__ == "__main__":
     main()
--- py2p/transform.py
+++ py2p/transform.py
@@ -1,12 +1,24 @@
-from py2p.io import load_suite2p_outputs
+import numpy as np
+
+def filter_rois(suite2p_data_output):
+    """
+    Filters the ROIs and neuropil fluorescence based on the cell identifier.
+    
+    Parameters:
+    suite2p_data_output (dict): Dictionary containing Suite2p output data.
+    
+    Returns:
+    tuple: Filtered ROIs and neuropil fluorescence.
+    """
+    # Assign bool T/F based on confidence index
+    true_cells_only = suite2p_data_output['cell_identifier'][:, 0].astype(bool)
+    
+    # Filter ROIs and neuropil based on bool value
+    filtered_roi = np.array(suite2p_data_output['roi_fluorescence'][true_cells_only])
+    filtered_neuropil = np.array(suite2p_data_output['neuropil_fluorescence'][true_cells_only])
+    
+    return true_cells_only, filtered_roi, filtered_neuropil
 
 
 
 
-# assign bool T/F based on confidence index
-true_cells_only = suite2p_data_output['cell_identifier'][:,0].astype(bool)
-print(true_cells_only)
-
-# filter ROIs and neuropil based on bool value
-filtered_roi = np.array(suite2p_data_output['roi_fluorescence'][true_cells_only])
-filtered_neuropil = np.array(suite2p_data_output['neuropil_fluorescence'][true_cells_only])
