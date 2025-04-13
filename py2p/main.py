ed64370c4135d58304f35161b22429ed60a98323
Sindhuja Baskar <153032345+sindhujabaskar@users.noreply.github.com>
2025-04-13 17:34:12 -0400

    Feat: added calculate dff function

--- py2p/main.py
+++ py2p/main.py
@@ -9,11 +9,14 @@ def main():
     filtered_data = filter_rois(data)
     #assert filtered_data[0].shape == data['roi_fluorescence'].shape, "Filtered data shape does not match original data shape."
     # Filtered data returns a tuple of (true_cells_only, filtered_roi, filtered_neuropil) where true_cells_only contains
-    # numpy booleans (not Python booleans) so we can use sum 
+    # numpy booleans (not Python booleans) so we can use sum - will treat as 1 and 0
     assert filtered_data[0].sum() == filtered_data[1].shape[0], "Filtered data does not match the number of true cells."
 
     baseline_fluorescence = calculate_baseline(filtered_data[1], percentile = 3)
-    assert baseline_fluorescence == filtered_data[1][1]  
+    #assert baseline_fluorescence == filtered_data[1][1]  
+
+    roi_dff = calculate_dff(filtered_data[1], baseline_fluorescence)
+    print(roi_dff)
 
 if __name__ == "__main__":
     main()
